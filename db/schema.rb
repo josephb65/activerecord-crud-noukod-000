4294beee4d68a3be17e4555772e38315c5359c2e
Billy Joseph <ingbillyjoseph@gmail.com>
2019-05-22 12:36:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_05_21_174353) do
+
   create_table "movies", force: :cascade do |t|
-      t.string "title"
-      t.integer "release_date"
-      t.string "director"
-      t.string "lead"
-      t.boolean "in_theaters"
-    end
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
